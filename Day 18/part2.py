48f8b39150820646f06f4dc8043ec6b38c4a1d89
Kurt D <kurtd5105@gmail.com>
2015-12-18 13:11:02 -0800

    Day 18 Solution added

---
+++ Day 18/part2.py
@@ -0,0 +1,96 @@
+import sys
+
+try:
+	with open(sys.argv[1]) as fileInput:
+		#Create a light array with each line as a sub-array where pound is 1 and . is 0
+		lightArray = [[1 if char == '#' else 0 for char in line.strip()] for line in fileInput]
+except:
+	print "File could not be opened."
+
+
+def edge(array, vertical, topLeft, i, j):
+	#If it is on the left or right side
+	if vertical:
+		if topLeft:
+			return (array[i - 1][j] + array[i - 1][j + 1] + array[i][j + 1]
+					+ array[i + 1][j + 1] + array[i + 1][j])
+		else:
+			return (array[i + 1][j] + array[i + 1][j - 1] + array[i][j - 1]
+					+ array[i - 1][j - 1] + array[i - 1][j])
+	#If it is on the top or bottom side
+	else:
+		if topLeft:
+			return (array[i][j + 1] + array[i + 1][j + 1] + array[i + 1][j]
+					+ array[i + 1][j - 1] + array[i][j - 1])
+		else:
+			return (array[i][j - 1] + array[i - 1][j - 1] + array[i - 1][j]
+					+ array[i - 1][j + 1] + array[i][j + 1])
+
+
+def middle(array, i, j):
+	return (array[i - 1][j] + array[i - 1][j + 1] + array[i][j + 1]
+			+ array[i + 1][j + 1] + array[i + 1][j] + array[i + 1][j - 1]
+			+ array[i][j - 1] + array[i - 1][j - 1])
+
+
+def animate(light, adjacentOn):
+	#Return the state of the light based on the game rules
+	if light:
+		if adjacentOn == 2 or adjacentOn == 3:
+			return 1
+		else:
+			return 0
+	else:
+		if adjacentOn == 3:
+			return 1
+		else:
+			return 0
+
+#Each corner will always be on
+lightArray[0][0] = 1
+lightArray[99][0] = 1
+lightArray[0][99] = 1
+lightArray[99][99] = 1
+
+prevArray = [list(array) for array in lightArray]
+for step in xrange(100):
+	for i in xrange(100):
+		for j in xrange(100):
+			adjacentOn = 0
+			#If in the first row
+			if i == 0:
+				#If on the top edge
+				if j != 0 and j != 99:
+					adjacentOn = edge(prevArray, 0, 1, i, j)
+					lightArray[i][j] = animate(prevArray[i][j], adjacentOn)
+
+			#If in the last row
+			elif i == 99:
+				#If on the bottom edge
+				if j != 0 and j != 99:
+					adjacentOn = edge(prevArray, 0, 0, i, j)
+					lightArray[i][j] = animate(prevArray[i][j], adjacentOn)
+
+			#If in any row between 0 and the end
+			else:
+				#If on the left edge
+				if j == 0:
+					adjacentOn = edge(prevArray, 1, 1, i, j)
+					lightArray[i][j] = animate(prevArray[i][j], adjacentOn)
+				#If on the right edge
+				elif j == 99:
+					adjacentOn = edge(prevArray, 1, 0, i, j)
+					lightArray[i][j] = animate(prevArray[i][j], adjacentOn)
+				#If surrounded by other lights
+				else:
+					adjacentOn = middle(prevArray, i, j)
+					lightArray[i][j] = animate(prevArray[i][j], adjacentOn)
+
+	prevArray = [list(array) for array in lightArray]
+
+
+total = 0
+for line in lightArray:
+	total += sum(line)
+
+print total
